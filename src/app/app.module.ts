78220d783e93d15d6349b7da535896e027bb7d91
Koch Toworksmile <korakoch@toworksmile.com>
2017-07-08 03:03:48 +0700

    reset ngx-bootstrap with some _variable.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BsDropdownModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
