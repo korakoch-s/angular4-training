42d011c88eaecb759739d8b1a0668ed962eabef8
Koch Toworksmile <korakoch@toworksmile.com>
2017-07-09 00:42:45 +0700

    Navbar not working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { DefaultComponent } from './features/default/default.component';
+
+export const appRoutes: Routes = [
+  { path: '', redirectTo: 'default', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'default', component: DefaultComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DefaultComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
     BrowserModule
   ],
   providers: [],
