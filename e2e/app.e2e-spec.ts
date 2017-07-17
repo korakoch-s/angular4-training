c46ad8ff1831adc4d250a3b79c022ff0ff763503
Koch Toworksmile <korakoch@toworksmile.com>
2017-07-17 15:37:57 +0700

    Edit first angular app.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular4-training App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toContain('Welcome');
   });
 });
