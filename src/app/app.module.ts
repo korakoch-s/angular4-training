8ccf85743cb9790c2f487faa759d3d013fc266c4
Koch Toworksmile <korakoch@toworksmile.com>
2017-07-17 15:53:38 +0700

    WIP: Setup bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JavascriptTypeComponent } from './topic/javascript-type/javascript-type.component';
+import { DemoAppComponent } from './demo-app/demo-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavascriptTypeComponent,
+    DemoAppComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/topic/javascript-type/javascript-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-javascript-type',
+  templateUrl: './javascript-type.component.html',
+  styleUrls: ['./javascript-type.component.scss']
+})
+export class JavascriptTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
