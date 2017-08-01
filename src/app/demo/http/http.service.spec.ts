bd9d273f8c36678999b387f7dcdb5de971a2192b
Koch Toworksmile <korakoch@toworksmile.com>
2017-08-01 14:16:17 +0700

    unit test http service.

--- src/app/demo/http/http.service.spec.ts
+++ src/app/demo/http/http.service.spec.ts
@@ -1,15 +1,77 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { MockBackend } from '@angular/http/testing';
+import { Http, ConnectionBackend, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpService]
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        HttpService,
+        { provide: 'apiUrl', useValue: 'http://mydomain.com/api/v1/posts' },
+        {
+          provide: Http,
+          useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
     });
   });
 
   it('should be created', inject([HttpService], (service: HttpService) => {
     expect(service).toBeTruthy();
   }));
+
+  // sets up an expectation that the correct URL will being requested
+  function expectURL(backend: MockBackend, url: string) {
+    backend.connections.subscribe(c => {
+      expect(c.request.url).toBe(url);
+      const response = new ResponseOptions({ body: '{"title": "post number 1"}' });
+      c.mockRespond(new Response(response));
+    });
+  }
+
+  describe('loadData', () => {
+    it('retrive data from given url',
+      inject([HttpService, MockBackend], fakeAsync((svc: HttpService, backend) => {
+        let res;
+        expectURL(backend, 'http://mydomain.com/api/v1/posts');
+        svc.loadData().subscribe((_res) => {
+          res = _res;
+        });
+        tick();
+        expect(res.title).toBe('post number 1');
+      }))
+    );
+  });
+
+  describe('makePost', () => {
+    it('post and retrive data from given url',
+      inject([HttpService, MockBackend], fakeAsync((svc: HttpService, backend: MockBackend) => {
+        let res;
+        // expectURL(backend, 'http://mydomain.com/api/v1/posts');
+        backend.connections.subscribe(c => {
+          expect(c.request.url).toBe('http://mydomain.com/api/v1/posts');
+          expect(c.request._body).toBe(JSON.stringify({
+            body: 'bar',
+            title: 'foo',
+            userId: 1
+          }));
+          const response = new ResponseOptions({ body: '{"id": "101"}' });
+          c.mockRespond(new Response(response));
+        });
+        svc.makePost('foo', 'bar').subscribe((_res) => {
+          res = _res;
+        });
+        tick();
+        expect(res.id).toBe('101');
+      }))
+    );
+  });
+
 });
---
+++ src/testunit.spec.ts
@@ -0,0 +1,5 @@
+describe ('Setup unit test framework check', () => {
+    it('1 equal to 1', () => {
+        expect(1).toEqual(1);
+    });
+});
